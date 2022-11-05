6abbe1b9d7f04554735a4e7581ff6b8ae606f758
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-05 09:11:23 -0300

    add: pages

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AboutPage ():React.ReactElement {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </div>
+  )
+}
